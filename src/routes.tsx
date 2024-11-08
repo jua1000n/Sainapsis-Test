0d13dca2357d42db2f9e5dee96a5cb5bd288570b
Juan <jua1000n@gmail.com>
2024-11-08 01:00:13 -0500

    update add new path order-management transition-logs

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import { UserInterface } from "./pages/UserInterface";
+import { OrderManagement } from "./pages/OrderManagement";
+import { TransitionLog } from "./pages/TransitionLog";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <UserInterface />,
+  },
+  {
+    path: "/order-management",
+    element: <OrderManagement />,
+  },
+  {
+    path: "/transition-logs",
+    element: <TransitionLog />,
   },
 ]);
