4949a5e57c4f18352a72ebe95d8c43e7b873d795
Juan <jua1000n@gmail.com>
2024-11-24 00:24:16 -0500

    update useOrder consult api

--- src/utils/hooks/useOrder.ts
+++ src/utils/hooks/useOrder.ts
@@ -1,7 +1,58 @@
-import useStore from "@/store/useStore";
+import { Order } from "@/types/Order";
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../config";
+import { OrderState } from "@/types/OrderState";
 
 export const useOrder = () => {
-  const orders = useStore((state) => state.orders);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  return { orders };
+  const updateOrderState = async (orderId: string, newState: OrderState) => {
+    try {
+      const url =
+        newState === OrderState.Canceled
+          ? `${API_BASE_URL}order/${orderId}/cancel/`
+          : `${API_BASE_URL}order/${orderId}/next-state/`;
+      const response = await fetch(url, { method: "PATCH" });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+
+      const data: Order = await response.json();
+      setOrders((prevOrders) => {
+        return prevOrders.map((order) =>
+          order.id === data.id ? { ...order, ...data } : order
+        );
+      });
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`${API_BASE_URL}order/orders/`);
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setOrders(data);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { orders, orderError: error, orderLoading: loading, updateOrderState };
 };
